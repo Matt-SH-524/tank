94f11cd259984aada71944865e6c506869a8e9a1
Tank0107 <13918247414@126.com>
2023-01-08 08:30:55 +0800

    添加键盘监听事件。

--- src/com/mashibing/tank/Main.java
+++ src/com/mashibing/tank/Main.java
@@ -1,10 +1,6 @@
 package com.mashibing.tank;
 
-import java.awt.Frame;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-public class T {
+public class Main {
     public static void main(String[] args) {
         //new个对象
         TankFrame tf = new TankFrame();
--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -2,11 +2,15 @@ package com.mashibing.tank;
 
 import java.awt.Graphics;
 import java.awt.Frame;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 //继承Frame类(窗口类)，优势：可以重写Frame方法
 public class TankFrame extends Frame {
+    //想要画面动，坐标就要是变量
+    int x=200,y=200;
 
     //建立一个构造方法
     public TankFrame() {
@@ -18,11 +22,16 @@ public class TankFrame extends Frame {
         setTitle("tank war");
         //Java窗口可见。
         setVisible(true);
+
+        //键盘监听处理类，这样通过键盘可以让画面动起来。
+        this.addKeyListener(new MyKeyListener());
+
         //添加一个window监听器，WindowAdapter()是匿名类。-没有命名的类
         addWindowListener(new WindowAdapter() {
-            //重写方法 Generate菜单里
+            //重写方法 在Generate菜单里
             @Override
             public void windowClosing(WindowEvent e) {
+                //windowClosing是窗口正在关。
                 //super.windowClosing(e); 修改如下
                 System.exit(0);
             }
@@ -36,6 +45,27 @@ public class TankFrame extends Frame {
     public void paint(Graphics g) {
         //这个方法没有被调用，却被打印出来了，说明它是自动调用的。
         //Graphics是画图的类，相当于一支画笔。
-        g.fillRect(200,200,50,50);
+        g.fillRect(x,y,50,50);
+        x+=10;
+        y+=10;
+    }
+
+    //内部类
+    class MyKeyListener extends KeyAdapter {
+        //这个类里面专门处理对键盘的监听
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            //一个键被按下去时候调用
+            System.out.println("keyPressed");
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            //一个键被释放时候调用
+            System.out.println("keyReleased");
+        }
+
+
     }
 }
